28aea949799c60cc39699b39879d08ef8d4003fb
Yeongcheol Kim <aree91@cnu.ac.kr>
2020-01-14 12:01:51 +0900

    Update GraderTest.java

--- How-to/Testing/Java/200114/gradleproject01/src/test/java/gradleproject01/GraderTest.java
+++ How-to/Testing/Java/200114/gradleproject01/src/test/java/gradleproject01/GraderTest.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 @DisplayName("학점 계산 프로그램 테스트")
+/* test method마다 instance가 하나씩 생성되는데 이 annotation을 사용하면 instance 하나로 모든 테스트케이스 수행 */
+//@TestInstance(Lifecycle.PER_CLASS)
 class GraderTest {
 	
 	Grader grader;
@@ -15,93 +22,120 @@ class GraderTest {
 	@BeforeEach
 	void setUp() {
 		grader = new Grader();
-		System.out.println("Test setup");
+//		System.out.println("Test setup");
 	}
 	
 	@AfterEach
 	void tearDown() {
-		grader = new Grader();
-		System.out.println("Test end");
+//		grader = new Grader();
+//		System.out.println("Test end");
 	}
 
-	@DisplayName("90점 이상이면 A 등급")
-	@Test
-	void should_give_A_for_students_with_more_than_90pts() {
-		/* Arrange */
-		double mids = 90;
-		double finals = 92;
-		double homeworks = 90;
-//		Grader grader = new Grader();
-		
-		/* Act */
-		Grade actual = grader.computeGrade(mids, finals, homeworks);
-		
-		/* Assert */
-		assertEquals(Grade.A, actual, "Should be A");
+	@DisplayName("정상적인 점수 입력")
+	@Nested
+	class WhenValidScoresEntered {
+		@DisplayName("90점 이상이면 A 등급")
+		@Test
+		void should_give_A_for_students_with_more_than_90pts() {
+			/* Arrange */
+			double mids = 90;
+			double finals = 92;
+			double homeworks = 90;
+
+			/* Act */
+			Grade actual = grader.computeGrade(mids, finals, homeworks);
+
+			/* Assert */
+			assertEquals(Grade.A, actual, "Should be A");
+		}
+
+		@DisplayName("80점 이상이면 B 등급")
+		@Test
+		void should_give_B_for_students_with_more_than_80pts() {
+			/* Arrange */
+			double mids = 80;
+			double finals = 92;
+			double homeworks = 90;
+
+			/* Act */
+			Grade actual = grader.computeGrade(mids, finals, homeworks);
+
+			/* Assert */
+			assertEquals(Grade.B, actual, "Should be B");
+		}
+
+		@DisplayName("70점 이상이면 C 등급")
+		@Test
+		void should_give_C_for_students_with_more_than_70pts() {
+			/* Arrange */
+			double mids = 80;
+			double finals = 72;
+			double homeworks = 70;
+
+			/* Act */
+			Grade actual = grader.computeGrade(mids, finals, homeworks);
+
+			/* Assert */
+			assertEquals(Grade.C, actual, "Should be C");
+		}
+
+		@DisplayName("60점 이상이면 D 등급")
+		@Test
+		void should_give_D_for_students_with_more_than_60pts() {
+			/* Arrange */
+			double mids = 60;
+			double finals = 62;
+			double homeworks = 70;
+
+			/* Act */
+			Grade actual = grader.computeGrade(mids, finals, homeworks);
+
+			/* Assert */
+			assertEquals(Grade.D, actual, "Should be D");
+		}
+
+		@DisplayName("60점 미만이면 F 등급")
+		@Test
+		void should_give_F_for_students_with_less_than_60pts() {
+			/* Arrange */
+			double mids = 50;
+			double finals = 92;
+			double homeworks = 30;
+
+			/* Act */
+			Grade actual = grader.computeGrade(mids, finals, homeworks);
+
+			/* Assert */
+			assertEquals(Grade.F, actual, "Should be F");
+		}
 	}
 	
-	@DisplayName("80점 이상이면 B 등급")
-	@Test
-	void should_give_B_for_students_with_more_than_80pts() {
-		/* Arrange */
-		double mids = 80;
-		double finals = 92;
-		double homeworks = 90;
-//		Grader grader = new Grader();
-		
-		/* Act */
-		Grade actual = grader.computeGrade(mids, finals, homeworks);
-		
-		/* Assert */
-		assertEquals(Grade.B, actual, "Should be B");
-	}
-	
-	@DisplayName("70점 이상이면 C 등급")
-	@Test
-	void should_give_C_for_students_with_more_than_70pts() {
-		/* Arrange */
-		double mids = 80;
-		double finals = 72;
-		double homeworks = 70;
-//		Grader grader = new Grader();
-		
-		/* Act */
-		Grade actual = grader.computeGrade(mids, finals, homeworks);
-		
-		/* Assert */
-		assertEquals(Grade.C, actual, "Should be C");
-	}
-	
-	@DisplayName("60점 이상이면 D 등급")
-	@Test
-	void should_give_D_for_students_with_more_than_60pts() {
-		/* Arrange */
-		double mids = 60;
-		double finals = 62;
-		double homeworks = 70;
-//		Grader grader = new Grader();
-		
-		/* Act */
-		Grade actual = grader.computeGrade(mids, finals, homeworks);
-		
-		/* Assert */
-		assertEquals(Grade.D, actual, "Should be D");
-	}
-	
-	@DisplayName("60점 미만이면 F 등급")
-	@Test
-	void should_give_F_for_students_with_less_than_60pts() {
-		/* Arrange */
-		double mids = 50;
-		double finals = 92;
-		double homeworks = 30;
-//		Grader grader = new Grader();
-		
-		/* Act */
-		Grade actual = grader.computeGrade(mids, finals, homeworks);
-		
-		/* Assert */
-		assertEquals(Grade.F, actual, "Should be F");
+	@DisplayName("비정상적인 점수 입력")
+	@Nested
+	class WhenInvalidScoresEntered {
+		@DisplayName("중간점수 100 초과 Too Big Score Exception")
+		@Test
+		void should_give_too_big_score_exception() {
+			double mids = 120;
+			double finals = 92;
+			double homeworks = 30;
+
+			assertThrows(InvalidRangeException.class, () -> {
+				grader.computeGrade(mids, finals, homeworks);
+			});
+		}
+
+		@DisplayName("중간점수 0 미만 Negative Score Exception")
+		@Test
+		void should_give_negative_score_exception() {
+			double mids = -100;
+			double finals = 44;
+			double homeworks = 23;
+
+			assertThrows(InvalidRangeException.class, () -> {
+				grader.computeGrade(mids, finals, homeworks);
+			});
+		}
 	}
 
 }
